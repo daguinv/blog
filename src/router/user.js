3a7e4a4b7b130629b04363fdf8138faf4a4fc9ea
daguinv <2641479340@qq.com>
2021-05-10 01:34:10 +0800

    设置cookie

--- src/router/user.js
+++ src/router/user.js
@@ -3,16 +3,33 @@ const { SuccessModel, ErrorModel } = require('../model/resModel')
 
 const handleUserRouter = (req, res) => {
   const method = req.method
-  if (method === 'POST' && req.path === '/api/user/login') {
-    let { username, password } = req.body
-    return login(username, password).then(res => {
-      if (res.username) {
-        return new SuccessModel(res)
+  if (method === 'GET' && req.path === '/api/user/login') {
+    // let { username, password } = req.body
+    let { username, password } = req.query
+    return login(username, password).then(data => {
+      if (data.username) {
+        res.setHeader('Set-Cookie', `username=${data.username}; path=/; httpOnly; expires=${getCookieExpires()}`)
+        return new SuccessModel(data)
       } else {
         return new ErrorModel("登录失败")
       }
     })
 
   }
+  // 登录验证的测试
+  if(method === 'GET' && req.path === '/api/user/login-test'){
+    if(req.cookie.username){
+      return Promise.resolve(new SuccessModel(req.cookie.username))
+    }
+    return new ErrorModel("登录失败")
+  }
 }
+
+const getCookieExpires = () => {
+  const d = new Date()
+  d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+  // 把Date对象转换成字符串
+  return d.toGMTString()
+}
+
 module.exports = handleUserRouter
--- src/serverHandle.js
+++ src/serverHandle.js
@@ -23,11 +23,24 @@ const getPostData = async (req) => {
   })
 }
 
+
 exports.serverHandle = (req, res) => {
   res.setHeader('Content-type', 'application/json')
   req.path = req.url.split("?")[0]
   req.query = querystring.parse(req.url.split('?')[1])
-
+  // 解析cookie
+  req.cookie = {}
+  const cookieStr = req.headers.cookie || ''
+  cookieStr.split(';').forEach(item => {
+    if(!item){
+      return 
+    }
+    const arr = item.split('=')
+    const key = arr[0].trim()
+    const value = arr[1].trim()
+    req.cookie[key] = value
+  });
+  console.log("cookie is", req.cookie);
   getPostData(req).then(postData => {
     req.body = postData
     console.log("body数据", req.body);
@@ -43,7 +56,7 @@ exports.serverHandle = (req, res) => {
     if (userData) {
       userData.then(userData => {
         res.end(JSON.stringify(userData))
-      })
+      })  
       return;
     }
 
